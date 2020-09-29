f3a9ff6455350f26c5215c8a97848b4647eb8988
fanzhizheng.norman <fanzhizheng.norman@bytedance.com>
2020-09-29 12:28:50 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./static/precache-manifest.e5b0bd8d54897df7ba6e98cb0ae10fb4.js"
+  "../static/precache-manifest.1d150e9d82e68fabe688d699720a733c.js"
 );
 
 self.addEventListener('message', (event) => {
