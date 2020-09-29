4001ad76ea16c32240fc8040a4ab3d8966ddc923
fanzhizheng.norman <fanzhizheng.norman@bytedance.com>
2020-09-29 11:31:35 +0800

    Updates

---
+++ precache-manifest.731331ecff27b1642f66601428c28503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bc6e92fdee217a625352eaa69b6e445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36fa7b7e13d253298bb6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "89940448c79308fb3978",
+    "url": "/static/js/2.90474471.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.90474471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36fa7b7e13d253298bb6",
+    "url": "/static/js/main.ec909bec.chunk.js"
+  },
+  {
+    "revision": "cc7eb8a97fc595fb1b3c",
+    "url": "/static/js/runtime-main.4e490c10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
